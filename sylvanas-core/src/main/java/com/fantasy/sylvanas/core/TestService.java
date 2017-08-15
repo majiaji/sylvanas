717ca63728aa181000d4610a5aebf4e09327b85e
jiaji.mjj <jiaji.mjj@hotmail.com>
2017-08-15 08:24:01 +0800

    update

---
+++ sylvanas-core/src/main/java/com/fantasy/sylvanas/core/TestService.java
@@ -0,0 +1,13 @@
+package com.fantasy.sylvanas.core;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jiaji on 2017/7/17.
+ */
+@Service
+public class TestService {
+    public void haha() {
+
+    }
+}
